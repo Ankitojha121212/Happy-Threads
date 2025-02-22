ac0670923d11cedd88942f000ee0cfd8ebe970f5
Ankit Ojha <Ankitojha121212@gmail.com>
2025-02-22 23:26:48 +0530

    working

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -53,6 +53,7 @@ return (
       <button className="bg-red-600 hover:bg-red-700 text-white py-2 px-4 rounded-lg transition-all duration-300">
         Shop Now
       </button>
+      
     </div>
 
     {/* Navigation dots */}
